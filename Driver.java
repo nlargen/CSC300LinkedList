6d4119538e7c30e63ceae876c26d156c9f22a5ad
nlargen <nicholas.largen@cuw.edu>
2015-02-25 23:31:47 -0600

    made a change

--- Driver.java
+++ Driver.java
@@ -5,12 +5,12 @@ public class Driver {
 	public static void main(String[] args) throws Exception
 	{
 		LinkedList ll = new LinkedList();
-		ll.addFront(2);
-		ll.addFront(3);
-		ll.addFront(4);
-		ll.addFront(5);
-		ll.addFront(6);
+		ll.addFront(1);
+		ll.addFront(8);
 		ll.addFront(7);
+		ll.addFront(3);
+		ll.addFront(5);
+		ll.addFront(9);
 		ll.display();
 		ll.displayInReverse();
 		
