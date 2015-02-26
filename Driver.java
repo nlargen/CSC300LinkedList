17c15bd7c9d92b5606a6fd5839754a0d2e0c113f
nlargen <nicholas.largen@cuw.edu>
2015-02-25 23:46:11 -0600

    made a change

--- Driver.java
+++ Driver.java
@@ -5,14 +5,16 @@ public class Driver {
 	public static void main(String[] args) throws Exception
 	{
 		LinkedList ll = new LinkedList();
-		ll.addFront(1);
-		ll.addFront(8);
-		ll.addFront(7);
-		ll.addFront(3);
-		ll.addFront(5);
-		ll.addFront(9);
-		ll.display();
-		ll.displayInReverse();
+		ll.pushstack(1);
+		ll.pushstack(4);
+		ll.pushstack(7);
+		ll.popstack();
+		ll.peekstack();
+		//ll.addFront(3);
+		//ll.addFront(5);
+		//ll.addFront(9);
+		//ll.display();
+		//ll.displayInReverse();
 		
 	}
 
--- LinkedList.java
+++ LinkedList.java
@@ -214,4 +214,56 @@ public class LinkedList
 			return curr.getPayload(); 
 		}
 	}
+	public void pushstack(int payload) throws Exception 
+	{
+
+		Node n = new Node(payload);
+		if(this.head == null)
+		{
+			this.head = n;
+			this.tail = n; 
+		}
+		else
+		{
+			
+			this.tail.setNextNode(n); 
+			n.setPrevNode(tail); 
+			this.tail = n; 
+		}
+		this.count++;
+	}
+	public int popstack() throws Exception
+	{
+		if(head == null)
+		{
+			throw new Exception("The list is empty!");
+		}
+		else if(this.count == 1)
+		{
+			return this.removeFront();
+		}
+		else
+		{
+			Node curr = tail; 
+			tail = curr.getPrevNode(); 
+			curr.setPrevNode(null);
+			tail.setNextNode(null);
+			this.count--; 
+			return curr.getPayload(); 
+		}
+	}
+	public void peekstack() throws Exception 
+	{
+		if(head == null)
+		{
+			System.out.println("Empty List");
+		}
+		else 
+		{
+			Node currNode = tail;  
+		System.out.println(currNode.getPayload());
+		}
+		
+		}
+		
 }
